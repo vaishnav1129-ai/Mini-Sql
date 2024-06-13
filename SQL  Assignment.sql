a046cb6ceeac005105433156f9b2b6926686dd49
J VAISHNAV MOHAN <138311202+vaishnav1129-ai@users.noreply.github.com>
2024-06-13 21:24:51 +0530

    Update SQL  Assignment.sql

--- SQL  Assignment.sql
+++ SQL  Assignment.sql
@@ -58,3 +58,34 @@ EmployeeID  int primary key,
 FirstName varchar(25) not null,
 LastName varchar(25) not null, 
 Salary int check (salary between 20000 and 100000));
+
+-- Consider a table named "Employees" with columns: EmployeeID, FirstName,
+-- LastName, and Age. Write an SQL query to retrieve the first name and last name of
+-- employees who are older than 30.
+
+create table employee(employeeID int, FirstName varchar(25), LastName varchar(25), age int);
+insert into employee values(1,'Sanjay','Ram',35);
+insert into employee values(2,'Siva','prastha',30);
+insert into employee values(3,'akshay','singh',32);
+insert into employee values(4,'suresh','kumar',37);
+insert into employee values(5,'jusrly','ran',28);
+insert into employee values(6,'nifra','gn',32);
+select FirstName , LastName from employee where age>30;
+
+-- Using the same "Employees" table, write an SQL query to retrieve the first name, last
+-- name, and age of employees whose age is between 20 and 30.
+select FirstName , LastName , age from employee where age  between 20 and 30 ;
+
+-- Given a table named "Products" with columns: ProductID, ProductName, Price, and
+-- InStock (0- for out of stock, 1- for in stock). Write an SQL query to retrieve the product
+-- names and prices of products that are either priced above $100 or are out of stock.
+
+create table products (productid int, productname varchar(34), price int, instock int CHECK ( instock=1 or instock = 0));
+insert into products values(1,'soap',120,0),(2,'handwash',140,1);
+select * from products;
+insert into products values(3,'chips',10,0),(4,'real furit juice',120,0);
+insert into products values(5,'butter',70,0),(6,'Apple pie',50,0);
+select * from products where price > 100 or instock = 0;
+-- Using the "Products" table, write an SQL query to retrieve the product names and
+-- prices of products that are in stock and priced between 50 and 150.
+select productname , price from products where price  between 50 and 150;
